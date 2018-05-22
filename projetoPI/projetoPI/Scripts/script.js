fffebba2b4bae4d99d4166d8f611ba6208d067e6
Luan Borges dos Santos <luanunai-mg@hotmail.com>
2018-05-22 15:41:15 -0300

    Validando double

--- projetoPI/projetoPI/Scripts/script.js
+++ projetoPI/projetoPI/Scripts/script.js
@@ -7,4 +7,18 @@
         $("#multiCollapseExample1").addClass('collapse')
             .removeClass('in');
     });
+
+    $("#valor_pago").keypress(function (e) {
+        if (e.which === 8 || e.which === 0) {
+            
+        } else {
+            e.preventDefault();
+            str = $(this).val();
+            if (e.which === 46) {
+                $("#valor_pago").val(str + ",");
+            } else {
+                $("#valor_pago").val(str + String.fromCharCode(e.which));
+            }
+        }
+    });
 }); 
